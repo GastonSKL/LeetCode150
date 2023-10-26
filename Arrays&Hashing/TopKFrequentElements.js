aaba7afe33fb429f1ba8a090d66b744272c22423
Gaston Skamlec <gastonlskamlec@gmail.com>
2023-10-26 15:21:26 -0300

    asd

--- Arrays&Hashing/TopKFrequentElements.js
+++ Arrays&Hashing/TopKFrequentElements.js
@@ -4,38 +4,6 @@ let nums = [1,1,1,2,2,3];
 let k = 2;
 // Output: [1,2]
 
-
-// const topKFrequent = (nums, k) => 
-// {
-//    let map = new Map();
-
-//    for(let i = 0; i < nums.length ; i++)
-//    {
-//         if(!map.has(nums[i]))
-//         {
-//             map.set(nums[i], 1);
-//         } else
-//         {
-//             map.set(nums[i], map.get(nums[i]) + 1);
-//         }
-//    }
-
-//    const mapSort = new Map([...map.entries()].sort((a,b) => b[1] - a[1]));
-
-//    let count = 0;
-//    let result = [];
-//    for(let[key, val] of mapSort.entries())
-//    {
-//         if(count < k)
-//         {
-//             result.push(key)
-//             count++;
-//         }
-//    }
-
-//    return result;
-// };
-
 const topKFrequent = (nums, k) => 
 {
    let map = new Map();
